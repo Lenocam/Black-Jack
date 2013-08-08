f52731aee781d218ab2a8e13db51f619cb65c39c
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-07 22:59:49 -0400

    experimenting to figure out how to move forward

--- BJack.rb
+++ BJack.rb
@@ -1,10 +1,22 @@
 suits = %w[hearts diamonds clubs spades]
 names = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
 deck = suits.product(names)
-p deck
-deck.shufle
-p deck
-	
+shuffled_deck = deck.shuffle 
+p shuffled_deck
+
+
+=begin
+do 2.times {player_cards = shuffled_deck.pop}
+puts
+p shuffled_deck
+puts
+p player_cards
+shuffled_deck.pop
+puts
+p shuffled_deck
+
+p 'do you want to hit Y for yes or N for No'
+=end
 
 
 
