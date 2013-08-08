0c9994b6c6c0b36caaebbc140ef516a4375a57d5
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-08 15:38:00 -0400

    wtf

--- pbj.rb
+++ pbj.rb
@@ -20,20 +20,21 @@ hand_value = 0
   player_array.each do |card|
     if card[1] == "A"
       hand_value += 11
-    elsif card[1].to_i == 2..10
+      
+    elsif card[1].to_i == (2..10)
       hand_value += card[1].to_i
+      binding.pry
     else 
       card[1] ==  'Q' || card[1] == 'J' || card[1] == 'K' 
       hand_value += 10
   end
 end
 
-
 says("player_array")
-p player_array
+puts player_array
 puts
 says("dealers_array")
-p dealers_array
+puts dealers_array
 puts
 
 puts hand_value
