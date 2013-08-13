fd7400e8bcf3fa44c06f7d200f25ae1af35b8642
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-13 16:18:53 -0400

    Beginning oo version of BJ.

---
+++ N&V.rb
@@ -0,0 +1,46 @@
+"Create a deck, player(s), cards(s)
+shuffle deck
+After the deck is shuffled I want to deal to to the dealer and a player.
+Both will get two cards to start with, but I'll need to be able to add more later.
+I'll want each card to be searchable by the 1 spot to go toward determining the value of the card.
+The value of the card will need to pre-determined before this point. So i'll have to develop a way 
+for the all the names on the cards to also have point values associated with them and for when
+an Ace should be considered 1 point or 11 points. I think the best way to know when an Ace should be 1 or 11 
+is to have the cards to be re-examined after everytime a player needs a new card. That way you don't assign a 
+value to an Ace to early then have to walk it back before the hand total has already passed 21.
+The rules are different for players and deal.
+for the player to win he needs to have more > than the dealer but less than or equal to =< 21. Tie goes to the player.
+Converserly, the dealer must have more > the player or less than or equal =< to 21 unless the player also has 21. But the dealr
+stops drawing cards if he has reached 17. I need to find a way to end the game cleanly with options to continue or to start a
+new game with a new deck. It would be fun to to bets and so forth, but that may be a little further down the road. Also, a game of
+war would be easy to make and might be a nice option to give a player when they start."
+
+
+
+puts "noun"
+puts "game"
+puts "new game"
+puts "dealer"
+puts "card"
+puts "blackjack"
+puts "casino"
+puts "table"
+puts "hand"
+puts "suits"
+puts "ace"
+puts "21"
+puts "value"
+puts "17"
+puts "discard pile"
+puts ""
+puts "verb"
+puts "hit"
+puts "stay"
+puts "house"
+puts "split"
+puts "deal"
+puts "bet"
+puts "pass"
+puts "turn"
+puts "shuffle"
+puts "discard"
