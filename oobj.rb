e85c6f3bacd4d311cb8e906a277a66bf37d71b79
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-14 12:54:38 -0400

    used solutions form help

--- oobj.rb
+++ oobj.rb
@@ -1,3 +1,4 @@
+
 #class Game
 #  def method_name
     
@@ -7,53 +8,100 @@
 class Card
   attr_accessor :suit, :face_value
     
-def initialize(suit, face_value)
-  @suit = suit
-  @face_value = face_value    
-end
-
-end
-
-class Deck < Card
-    suits      = %w[hearts diamonds clubs spades]
-    face_value = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
-  
-  def initialize (suit, face_value)
+  def initialize(suit, face_value)
     @suit = suit
-    @face_value = face_value
-    
+    @face_value = face_value    
   end
-  def create_deck
-    decked = suits.product(face_value)
+  def output
+    puts "Suit = #{find_suit}, Value: #{face_value}"
   end
-end
-p Deck.create_deck
 
-#Deck = 52 Card
-#Card = suit & face_value
-  #suit = %w[Hearts Diamonds Clubs Spades]
-  #face_value = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
-=begin
+  def to_s
+    output
+  end
+
+  def find_suit
+    ret_val = case suit
+      when 'H' then 'Hearts'
+      when 'D' then 'Diamonds'
+      when 'S' then 'Spades'
+      when 'C' then 'Clubs'
+    end
+    ret_val
+  end      
+end
+
 class Deck
+  attr_accessor :cards
 
+  def initialize
+    @cards = []
+    %w[H D C S].each do |suit|
+    %w[A 2 3 4 5 6 7 8 9 10 J Q K].each do |face_value|
+      @cards << Card.new(suit, face_value)
+    end
+  end
+    scramble!
+  end
+
+  def scramble!
+    cards.shuffle!
+  end
+
+  def deal_one
+    cards.pop
+  end
+
+  def size
+    cards.size
+  end
 end
+
+
+deck = Deck.new
+
+
+puts deck.cards
+puts deck.size
+deck.deal_one.to_s
+deck.deal_one.to_s
+deck.deal_one.to_s
+deck.deal_one.to_s
+deck.deal_one.to_s
+deck.deal_one.to_s
+deck.deal_one.to_s
+
+puts deck.size
+
+
+
 
 
 
 class Player
+  #has hand
+  #hits
+  #stays
 
 end
+
 class Dealer
+  #has hand
+  #hits
+  #stays
 
 end
 
 class Hand
+  #has cards from deck
+  #calculated
+  #added to
+    #at beginning of hand
+    #in hand based on choices from player(s)
+    
 
 end
 
 class Table
 
 end
-
-c1 = Card.new
-=end
