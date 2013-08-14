569977daa60a535e9f5cf9c41151164d7880fbae
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-14 09:27:59 -0400

    Still trying to create a deck with cards.

--- oobj.rb
+++ oobj.rb
@@ -1,31 +1,46 @@
-class Cards
-    attr_accessor :suit :face_value
-    def initialize(suit, face_value)
-        @suit = suit
-        @face_value     
-    end
-
+class Game
+  def method_name
+    
+  end
 end
+
+class Card
+  attr_accessor :suit :face_value
+    
+def initialize(suit, face_value)
+  @suit = suit
+  @face_value = face_value    
+end
+  
+end
+
 class Deck
-    include Cards
-    def initialize
+  def initialize
         
         
     end
 end
 class Player
-    def method_name
+  def method_name
         
-    end
+  end
 end
 class Dealer
-    def method_name
+  def method_name
         
-    end
+  end
 end
 
 class Hand
-    def method_name
+  def method_name
         
-    end
+  end
 end
+
+class Table
+  def method_name
+        
+  end
+end
+
+c1 = Card.new
