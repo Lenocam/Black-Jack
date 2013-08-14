dff2358c3165b1f960247d48f182cc382da1b6e6
Reed S. MacConnell <learn22ruby@gmail.com>
2013-08-14 10:23:51 -0400

    Working on deck. Still don't have it.

--- oobj.rb
+++ oobj.rb
@@ -1,46 +1,59 @@
-class Game
-  def method_name
+#class Game
+#  def method_name
     
-  end
-end
+#  end
+#end
 
 class Card
-  attr_accessor :suit :face_value
+  attr_accessor :suit, :face_value
     
 def initialize(suit, face_value)
   @suit = suit
   @face_value = face_value    
 end
-  
+
 end
 
-class Deck
-  def initialize
-        
-        
-    end
-end
-class Player
-  def method_name
-        
+class Deck < Card
+    suits      = %w[hearts diamonds clubs spades]
+    face_value = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
+  
+  def initialize (suit, face_value)
+    @suit = suit
+    @face_value = face_value
+    
   end
+  def create_deck
+    decked = suits.product(face_value)
+  end
+end
+p Deck.create_deck
+
+#Deck = 52 Card
+#Card = suit & face_value
+  #suit = %w[Hearts Diamonds Clubs Spades]
+  #face_value = %w[A 2 3 4 5 6 7 8 9 10 J Q K]
+=begin
+class Deck
+
+end
+
+
+
+class Player
+
 end
 class Dealer
-  def method_name
-        
-  end
+
 end
 
 class Hand
-  def method_name
-        
-  end
+
 end
 
 class Table
-  def method_name
-        
-  end
+
 end
 
 c1 = Card.new
+=end
